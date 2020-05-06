50d38fb546254d469823c6b3dcfd5ee3a1516484
Sainthedgehog1 <60622449+Sainthedgehog1@users.noreply.github.com>
2020-05-06 21:25:38 +0300

    доделал подвал, исправил контаинеры и медиа запросы, пробую подключить js

--- gulpfile.js
+++ gulpfile.js
@@ -97,5 +97,6 @@ gulp.task("clean", function () {
   return del("build");
 });
 
+
 gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
 gulp.task("start", gulp.series("build", "server"));
--- source/js/main.js
+++ source/js/main.js
@@ -1,34 +1,36 @@
+console.log(multiple(4, 2));
+
 // Модальное окно
 
 // открыть по кнопке
-$('.js-button-campaign').click(function() { 
-	
+$('.js-button-campaign').click(function() {
+
 	$('.js-overlay-campaign').fadeIn();
 	$('.js-overlay-campaign').addClass('disabled');
 });
 
 // закрыть на крестик
-$('.js-close-campaign').click(function() { 
+$('.js-close-campaign').click(function() {
 	$('.js-overlay-campaign').fadeOut();
-	
+
 });
 
 // закрыть по клику вне окна
-$(document).mouseup(function (e) { 
+$(document).mouseup(function (e) {
 	var popup = $('.js-popup-campaign');
 	if (e.target!=popup[0]&&popup.has(e.target).length === 0){
 		$('.js-overlay-campaign').fadeOut();
-		
+
 	}
 });
 
-// открыть по таймеру 
-$(window).on('load', function () { 
+// открыть по таймеру
+$(window).on('load', function () {
 	setTimeout(function(){
 		if($('.js-overlay-campaign').hasClass('disabled')) {
 			return false;
 		} else {
-			
+
 			$(".js-overlay-campaign").fadeIn();
 		}
 	}, 5000);
