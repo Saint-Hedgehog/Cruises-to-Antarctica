99e9c2b67b0ef75fdf0971347621a02a2c0385b7
Sainthedgehog1 <60622449+Sainthedgehog1@users.noreply.github.com>
2020-05-20 15:22:08 +0300

    jioj

--- source/js/main.js
+++ source/js/main.js
@@ -1,28 +1,51 @@
 'use strict';
-var element = document.getElementById('phone');
-var maskOptions = {
-  mask: '+0(000)000-00-00',
-  lazy: false
-};
-var mask = new IMask(element, maskOptions);
+var pageHeader = document.querySelector('.page-header');
+var headerToggle = document.querySelector('.page-header__toggle');
 
-// Модальное окно
+pageHeader.classList.remove('page-header--nojs');
 
-// открыть по кнопке
-('.js-button-campaign').click (function () {
-  ('.js-overlay-campaign').fadeIn();
-  ('.js-overlay-campaign').addClass('disabled');
+headerToggle.addEventListener('click', function () {
+  if (pageHeader.classList.contains('page-header--closed')) {
+    pageHeader.classList.remove('page-header--closed');
+    pageHeader.classList.add('page-header--opened');
+  } else {
+    pageHeader.classList.add('page-header--closed');
+    pageHeader.classList.remove('page-header--opened');
+  }
 });
 
+//Маска для телефона
+var element = document.getElementById("phone");
+  var maskOptions = {
+      mask: "+0(000)000-00-00",
+      lazy: false
+  }
+  var mask = new IMask(element, maskOptions);
+
+
+
+    // Модальное окно
+
+// открыть по кнопке
+$(".js-button-campaign").click(function() {
+
+  $(".js-overlay-campaign").fadeIn();
+  $(".js-overlay-campaign").addClass("disabled");
+  window.onload = function(){
+    $(".js-overlay-campaign").removeClass("popap-display");
+  });
+    };
+
 // закрыть на крестик
-('.js-close-campaign').click (function () {
-  ('.js-overlay-campaign').fadeOut();
+$(".js-close-campaign").click(function() {
+  $(".js-overlay-campaign").fadeOut();
+
 });
 
 // закрыть по клику вне окна
-(document).mouseup(function (e) {
-  var popup = ('.js-popup-campaign');
-  if (e.target !== [0] && popup.has (e.target).length === 0) {
-    ('.js-overlay-campaign').fadeOut();
+$(document).mouseup(function (e) {
+  var popup = $(".js-popup-campaign");
+  if (e.target!=popup[0]&&popup.has(e.target).length === 0){
+    $(".js-overlay-campaign").fadeOut();
   }
 });
